db370842e1bceee4b275aa89ff363c0250eac3da
Brian Addicks <brian@addicks.us>
2015-07-20 21:55:51 -0400

    added New-ExVisioK6

--- src/cmdlets/New-ExVisioK6.ps1
+++ src/cmdlets/New-ExVisioK6.ps1
@@ -1,4 +1,4 @@
-function New-ExVisioS4 {
+function New-ExVisioK6 {
     [CmdletBinding()]
 	<#
         .SYNOPSIS
@@ -20,7 +20,7 @@ function New-ExVisioS4 {
 	)
 	
 	# Used for Write-Verbose and Throw
-	$VerbosePrefix = "New-ExVisioS4:"
+	$VerbosePrefix = "New-ExVisioK6:"
 	
 	# Test for VisioShell
 	$IsModuleAvailable = Get-Module -Listavailable -Name VisioShell
@@ -32,29 +32,24 @@ function New-ExVisioS4 {
 	
 	# Create objects for blade/psu placement
 	$BladeCoords = @()
-	$BladeCoords += HelperBladeProps slot1 5.3125 7.546875 8.75 7.1875 2.6349 7.534 4.2974 0.71
-	$BladeCoords += HelperBladeProps slot2 5.3125 8.25 8.75 7.890625 2.6349 8.2455 4.2974 0.71
-	$BladeCoords += HelperBladeProps slot3 5.3125 8.960938 8.75 8.601563 2.6339 8.9559 4.2974 0.71
-	$BladeCoords += HelperBladeProps slot4 5.3125 9.671875 8.75 9.3125 2.6388 9.66 4.2974 0.71
-	$BladeCoords += HelperBladeProps slot1_ul 5.3125 7.890625 7 7.546875 1.6356 7.7033 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot1_ur 7.0625 7.890625 8.75 7.546875 3.6298 7.7029 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot2_ul 5.3125 8.601563 7 8.25 1.6356 8.4148 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot2_ur 7.0625 8.601563 8.75 8.25 3.6298 8.4143 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot3_ul 5.3125 9.3125 7 8.960938 1.6346 9.1252 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot3_ur 7.0625 9.3125 8.75 8.960938 3.6288 9.1247 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot4_ul 5.3125 10.015625 7 9.671875 1.6395 9.8293 1.9423 0.213
-	$BladeCoords += HelperBladeProps slot4_ur 7.0625 10.015625 8.75 9.671875 3.6337 9.8288 1.9423 0.213
-	$BladeCoords += HelperBladeProps ps1 0.5625 6.5625 1.625 6.3125 1.0928 6.8546 1.2783 0.4913
-	$BladeCoords += HelperBladeProps ps2 1.703125 6.5625 2.765625 6.3125 2.2314 6.8541 1.2783 0.4913
-	$BladeCoords += HelperBladeProps ps3 2.84375 6.5625 3.90625 6.3125 3.3691 6.8522 1.2783 0.4913
-	$BladeCoords += HelperBladeProps ps4 3.984375 6.5625 5.046875 6.3125 4.5137 6.8541 1.2783 0.4913
+	$BladeCoords += HelperBladeProps slot1 0.25 9.8125 2.0625 9.4375 3.3564 9.6289
+	$BladeCoords += HelperBladeProps slot2 7.09375 9.8125 8.90625 9.4375 5.4853 9.6289
+	$BladeCoords += HelperBladeProps slot3 0.25 9.390625 2.0625 9.015625 3.3564 9.207
+	$BladeCoords += HelperBladeProps slot4 7.09375 9.390625 8.90625 9.015625 5.4853 9.207
+	$BladeCoords += HelperBladeProps slot5 0.25 8.96875 2.0625 8.59375 3.3564 8.7851
+	$BladeCoords += HelperBladeProps slot6 7.09375 8.96875 8.90625 8.59375 5.4853 8.7851
+	$BladeCoords += HelperBladeProps slot7 7.09375 10.28125 8.90625 9.90625 4.4195 10.0946
+	$BladeCoords += HelperBladeProps ps1 2.359375 8.0625 3.421875 7.6875 2.8957 8.321
+	$BladeCoords += HelperBladeProps ps2 3.5 8.0625 4.5625 7.6875 4.0304 8.321
+	$BladeCoords += HelperBladeProps ps3 4.632813 8.0625 5.695313 7.6875 5.1632 8.321
+	$BladeCoords += HelperBladeProps ps4 5.765625 8.0625 6.828125 7.6875 6.298 8.321
 	
 	# Start Visio
 	Write-Verbose "$VerbosePrefix Starting Visio"
 	Start-Visio -Quiet
 	
 	# Load Stencil
-	$StencilPath = "$PSScriptRoot\Resources\S-Series.vss"
+	$StencilPath = "$PSScriptRoot\Resources\K-Series.vss"
 	Write-Verbose "$VerbosePrefix Importing Stencil: $StencilPath"
 	$LoadStencil = Import-VisioStencilFile $StencilPath
 	
@@ -66,14 +61,14 @@ function New-ExVisioS4 {
 	
 	# Chassis Caption
 	Write-Verbose "$VerbosePrefix Creating chassis caption"
-	$Caption = Add-VisioRectangle 0.265625 10.75 5.328125 10.375 -Textbox -FontSize 18 -Text $ChassisModel
+	$Caption = Add-VisioRectangle 2.09375 10.75 7.09375 10.375 -Textbox -FontSize 18 -Text $ChassisModel
 		
 	# Chassis Stencil
 	$Stencil = Select-VisioStencil $ChassisModel
-	$Shape   = Add-VisioStencil $Stencil 2.7984 8.4697
+	$Shape   = Add-VisioStencil $Stencil 4.5863 9.2168
 
 	# Hostname Caption
-	$Caption = Add-VisioRectangle 0.265625 6.3125 5.328125 5.9375 -Textbox -FontSize 18 -Text $ChassisName
+	$Caption = Add-VisioRectangle 2.09375 7.6875 7.09375 7.3125 -Textbox -FontSize 18 -Text $ChassisName
 	
 	###########################################################################################
 	# Blades and OptionModules
@@ -90,32 +85,6 @@ function New-ExVisioS4 {
 		# Select and Drop Blade Stencil
 		$Stencil = Select-VisioStencil $s.Model
 		$Shape   = Add-VisioStencil $Stencil $b.pinx $b.piny
-		
-		# OptionModules
-		foreach ($o in $s.OptionModules) {
-			$OptionSlot = $o.Location.Split()
-			$OptionLoc  = $OptionSlot[0].SubString(0,1) + $OptionSlot[1].SubString(0,1)
-			$OptionSlot = $Slot + '_' + $OptionLoc
-			Write-Verbose "$VerbosePrefex $Slot"
-			$b = $BladeCoords | ? { $_.slot -eq $OptionSlot }
-			
-			# Create Caption
-			$Caption = Add-VisioRectangle $b.x1 $b.y1 $b.x2 $b.y2 -Textbox -FontSize 18 -Text $o.Model
-			
-			# Change color for option Slots
-			switch ($OptionLoc) {
-				'ul' {
-					Set-VisioShapeFont $Caption -ColorInHex C00000
-				}
-				'ur' {
-					Set-VisioShapeFont $Caption -ColorInHex 0070C0
-				}
-			}
-				
-			# Select and Drop Blade Stencil
-			$Stencil = Select-VisioStencil $o.Model
-			$Shape   = Add-VisioStencil $Stencil $b.pinx $b.piny
-		}
 	}
 	
 	###########################################################################################
